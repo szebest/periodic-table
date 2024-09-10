5d3e4ff878a5d4777d1a7c523c7ab75ef10f1519
Mateusz Szebestik <szebestik@gmail.com>
2024-09-10 14:47:16 +0200

    #1 Basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DefaultLayoutComponent } from './layout';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      {
+        path: '',
+        // TODO: add default view here, for now it is just AppComponent
+        component: AppComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
