27eb611644c2dc814cee9427d02749a9718b5ec6
Mateusz Szebestik <szebestik@gmail.com>
2024-09-12 12:34:10 +0200

    Added a divider to the bottom of the header

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, RouterModule],
+  imports: [MatToolbarModule, MatDividerModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
